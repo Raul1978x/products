ee47db2e048df3673f8097a0039f912c8c9ef6e2
Raul1978x <108808330+Raul1978x@users.noreply.github.com>
2024-07-31 22:18:30 -0300

    add .env & joi for validator

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { envs } from './products/config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,8 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     })
     );
-  await app.listen(3000);
+  await app.listen(envs.port);
+  console.log(`server running on port: ${envs.port}`);
+  
 }
 bootstrap();
